30d5cbd12a606939841577fcfffcbff406015621
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-01-15 20:30:21 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+Import os,
+
+Bot = Client(
+    "Payment-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+START_TEXT = """ Hai Dear {},
+Iam a payment bot😁.Do you want to make your own Payment bot?
+Use public repo Maintained by: [JOKER TG](https://t.me/IAM_A_JOKER)
+[Click here to Pay](https://t.me/IAM_A_JOKER)
+"""
+
+
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('❤ Source', url=f"https://github.com/Akbotmakersnew/Payment-bot"), 
+        InlineKeyboardButton('👥 Support', url=f"https://t.me/Ls_Supportz")
+        ]]
+    )
+
+Bot.run()
